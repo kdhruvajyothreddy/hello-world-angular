226356297028d3b594a912e103ffde43c47aff5f
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-11-17 13:22:01 +0530

    Including Components, generating components using angular cli, templates and directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
