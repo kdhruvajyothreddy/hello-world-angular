e58320ffea2446fe64cbe12f604208a1b5548f31
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-11-18 22:28:47 +0530

    Event binding & gather additional info about the event

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,12 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector: 'courses', // <courses>
     template: `
-        <button class="btn btn-primary" (click)="onSave()">Save</button> <!--Event based binding by calling a method-->
+        <button class="btn btn-primary" (click)="onSave($event)">Save</button> 
+        <!-- Event based binding and gathering additional information -->
         `
 })
 export class CoursesComponent {
-    onSave() { // Adding a method to be called at front end for event binding
-        console.log("Button was clicked");
+    onSave($event) { // Event based binding and passing additional information about the event
+        console.log("Button was clicked", $event);
     }
 }
