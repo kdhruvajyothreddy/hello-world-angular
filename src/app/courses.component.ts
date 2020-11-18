9ccb8ea2fcc4ed65b6ed032c103e68f702cd6c32
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-11-18 22:55:47 +0530

    To display event bubbling in event binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,12 +4,18 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector: 'courses', // <courses>
     template: `
-        <button class="btn btn-primary" (click)="onSave($event)">Save</button> 
-        <!-- Event based binding and gathering additional information -->
+        <div (click)="onDivClicked($event)" > <!-- Event bubbling in event binding -->
+            <button class="btn btn-primary" (click)="onSave($event)">Save</button> 
+            <!-- Event based binding and gathering additional information -->
+        </div>
         `
 })
 export class CoursesComponent {
     onSave($event) { // Event based binding and passing additional information about the event
         console.log("Button was clicked", $event);
+    }
+    onDivClicked($event) { // Passing event to display event bubbling in event binding
+        console.log("Button was clicked in Div", $event);
+        
     }
 }
